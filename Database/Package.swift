d5e28dc3473ba462803d466274e5cc0c947796dc
Dennis Müller <d3mueller@me.com>
2025-04-23 21:37:32 +0200

    Add GRDB Custom

--- Database/Package.swift
+++ Database/Package.swift
@@ -1,21 +1,25 @@
 // swift-tools-version: 6.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "Database",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "Database",
-            targets: ["Database"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Database"),
-
-    ]
+  name: "Database",
+  products: [
+    .library(
+      name: "Database",
+      targets: ["Database"]
+    )
+  ],
+  targets: [
+    .target(
+      name: "Database",
+      dependencies: [
+        "GRDB"
+      ]
+    ),
+    .binaryTarget(
+      name: "GRDB",
+      path: "../Generated/GRDB.xcframework" // Path relative to the Package.swift file
+    ),
+  ]
 )
--- Database/Sources/Database/Database.swift
+++ Database/Sources/Database/Database.swift
@@ -1,2 +1,8 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import GRDB
+
+public func test() async throws {
+  let pool = try DatabasePool(path: "")
+}
