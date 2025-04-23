13f90188ac70f1fa0d4af3ca859f7a78fc2ba836
Dennis Müller <d3mueller@me.com>
2025-04-23 21:26:43 +0200

    Initial Commit

---
+++ GRDBCustomExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GRDBCustomExample
+//
+//  Created by Dennis Müller on 23.04.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
